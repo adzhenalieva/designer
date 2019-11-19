b31fcd0684ce30f50be07c9882576f5c49c5e856
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-11-19 10:55:39 +0600

    link to basic api url added, main structureof the app added to App.js, styles changed

--- src/store/action.js
+++ src/store/action.js
@@ -10,18 +10,16 @@ export const listingSuccess = response => ({type: LISTING_SUCCESS, response});
 export const listingFailure = error => ({type: LISTING_FAILURE, error});
 
 
-export const fetchListings = number => {
+export const fetchListings = () => {
     return async dispatch => {
-        if (parseInt(number) <= 0 || parseInt(number) > 10) {
-
-        } else {
             dispatch(listingRequest());
             try {
-                const response = await axios.get('/listings?size=' + number);
-                dispatch(listingSuccess(response.data.results));
+                const response = await axios.get('/images.json');
+                dispatch(listingSuccess(response.data));
+                console.log(response.data);
             } catch (e) {
                 dispatch(listingFailure(e));
             }
-        }
+
     }
 };
